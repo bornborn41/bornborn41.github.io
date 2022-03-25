6752e76bb723d2b18d8615304fd86e283fa56967
bornborn41 <kriengkrai.yo.60@ubu.ac.th>
2022-03-26 00:06:45 +0700

    addmenu

--- js/menu.js
+++ js/menu.js
@@ -3,8 +3,10 @@ function backgroundMenu() {
     var checkBox = document.getElementById("toggle-btn");
     var div = document.getElementById("top");
     // var bg = document.body;
+    let body = document.body;
 
     if (checkBox.checked === true) {
+        
         let div = document.createElement('div');
         div.id = 'top';
         document.getElementById("addtop").appendChild(div);
@@ -12,8 +14,11 @@ function backgroundMenu() {
         div.style.zIndex = "1"
         div.style.width = "100%"
         div.style.height = "100%"
-        div.style.background = "rgba(0, 0, 0, 0.90)"
+        div.style.background = "rgba(0, 0, 0, 0.92)"
+        body.style.overflow = "hidden"
+
     } else {
         document.getElementById("top").remove();
+        body.style.overflow = ""
     }
 }
