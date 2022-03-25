dbb9e7a3626013def9562a2f230a89e086b8da86
bornborn41 <kriengkrai.yo.60@ubu.ac.th>
2022-03-25 23:54:28 +0700

    addmenu

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,19 @@
 
-// function backgroundMenu() {
-//     var checkBox = document.getElementById("toggle-btn");
-//     var div = document.getElementById("top");
-//     // var bg = document.body;
+function backgroundMenu() {
+    var checkBox = document.getElementById("toggle-btn");
+    var div = document.getElementById("top");
+    // var bg = document.body;
 
-//     if (checkBox.checked === true) {
-//         let div = document.createElement('div');
-//         div.id = 'top';
-//         document.getElementById("addtop").appendChild(div);
-//         div.style.position = "fixed"
-//         div.style.zIndex = "2"
-//         div.style.width = "100%"
-//         div.style.height = "100%"
-//         div.style.background = "rgba(0, 0, 0, 0.50)"
-//     } else {
-//         document.getElementById("top").remove();
-//     }
-// }
+    if (checkBox.checked === true) {
+        let div = document.createElement('div');
+        div.id = 'top';
+        document.getElementById("addtop").appendChild(div);
+        div.style.position = "fixed"
+        div.style.zIndex = "1"
+        div.style.width = "100%"
+        div.style.height = "100%"
+        div.style.background = "rgba(0, 0, 0, 0.90)"
+    } else {
+        document.getElementById("top").remove();
+    }
+}
