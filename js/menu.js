7233be9b2cdcd30a3da17106471c8483d789a5c1
bornborn41 <kriengkrai.yo.60@ubu.ac.th>
2022-03-25 23:52:38 +0700

    portfolio2

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+
+// function backgroundMenu() {
+//     var checkBox = document.getElementById("toggle-btn");
+//     var div = document.getElementById("top");
+//     // var bg = document.body;
+
+//     if (checkBox.checked === true) {
+//         let div = document.createElement('div');
+//         div.id = 'top';
+//         document.getElementById("addtop").appendChild(div);
+//         div.style.position = "fixed"
+//         div.style.zIndex = "2"
+//         div.style.width = "100%"
+//         div.style.height = "100%"
+//         div.style.background = "rgba(0, 0, 0, 0.50)"
+//     } else {
+//         document.getElementById("top").remove();
+//     }
+// }
